00091095c69f83eb7bc1310ad1eac1e398dc18fa
HeroSprsoNsa <aedhealt@gmail.com>
2026-06-19 13:43:24 +0700

    init: tugas ci4

---
+++ app/Database/Migrations/2026-06-11-174807_Product.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Product extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            "id" => [
+                "type" => "INT",
+                "constraint" => 11,
+                "unsigned" => true,
+                "auto_increment" => true,
+            ],
+            "nama" => [
+                "type" => "VARCHAR",
+                "constraint" => 255,
+                "null" => false,
+            ],
+            "harga" => [
+                "type" => "DOUBLE",
+                "null" => false,
+            ],
+            "jumlah" => [
+                "type" => "INT",
+                "constraint" => 5,
+                "null" => false,
+            ],
+            "foto" => [
+                "type" => "VARCHAR",
+                "constraint" => 255,
+            ],
+            "created_at" => [
+                "type" => "datetime",
+                "null" => true,
+            ],
+            "updated_at" => [
+                "type" => "datetime",
+                "null" => true,
+            ],
+        ]);
+
+        $this->forge->addKey("id", true);
+        $this->forge->createTable("product");
+    }
+
+    //--------------------------------------------------------------------
+
+    public function down()
+    {
+        $this->forge->dropTable("product", true);
+    }
+}
---
+++ app/Database/Migrations/2026-06-11-174827_TransactionDetail.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TransactionDetail extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            "id" => [
+                "type" => "INT",
+                "constraint" => 11,
+                "unsigned" => true,
+                "auto_increment" => true,
+            ],
+            "transaction_id" => [
+                "type" => "INT",
+                "constraint" => 11,
+                "unsigned" => true,
+            ],
+            "product_id" => [
+                "type" => "INT",
+                "constraint" => 11,
+                "unsigned" => true,
+            ],
+            "jumlah" => [
+                "type" => "INT",
+                "constraint" => 5,
+                "null" => false,
+            ],
+            "diskon" => [
+                "type" => "DOUBLE",
+                "null" => true,
+            ],
+            "subtotal_harga" => [
+                "type" => "DOUBLE",
+                "null" => false,
+            ],
+            "created_at" => [
+                "type" => "datetime",
+                "null" => true,
+            ],
+            "updated_at" => [
+                "type" => "datetime",
+                "null" => true,
+            ],
+        ]);
+
+        $this->forge->addKey("id", true);
+        $this->forge->createTable("transaction_detail");
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable("transaction_detail", true);
+    }
+}
